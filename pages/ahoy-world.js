e801319963479814c46c2e2c14df916eede7f94e
Alec Rice <alec.rice@moatmetrics.com>
2025-01-14 17:28:53 -0800

    Add template clickup custom command

---
+++ pages/ahoy-world.js
@@ -0,0 +1,26 @@
+module.exports = async (req, res) => {
+    const response = {
+      view: {
+        type: "list",
+        options: [
+          {
+            title: "Open Slapdash",
+            action: {
+              type: "open-url",
+              url: "https://slapdash.com"
+            }
+          },
+          {
+            title: "Copy Heart Emoji",
+            action: {
+              type: "copy",
+              value: "❤️"
+            }
+          }
+        ]
+      }
+    };
+    res.setHeader("Access-Control-Allow-Headers", "*"); // for config headers
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(response);
+  }
